699a3899aae940b7ac0d8dff3616a03194aaab27
fanjh <1396778999@qq.com>
2017-04-16 18:27:16 +0800

    增加移动端项目

--- 黑蝠社/images/usermes.js
+++
@@ -1,8 +0,0 @@
-document.writeln("<script language=\"javascript\"  src=\"/public/Js/jqbase.js\"></script>");
-document.writeln("<script language=\"javascript\" src=\"/public/xajax/index.js\"></script>");
-document.writeln("<script language=\"javascript\" src=\"/public/pagejs/MSClass.js\"></script>");
-document.writeln("<script language=\"javascript\" src=\"/public/pagejs/jquery-1.7.2.min.js\"></script>");  
-document.writeln("<script language=\"javascript\" src=\"/public/pagejs/jquerypop.js\"></script>");
-document.writeln("<script language=\"javascript\" src=\"/public/msgWin/divWin2.js\"></script>");
-document.writeln("<script language=\"javascript\" src=\"/public/pagejs/usermespub.js\"></script>");
-document.writeln("<link href=\"/public/msgWin/DivWin_page.css\" rel=\"stylesheet\" type=\"text/css\" \/>");
